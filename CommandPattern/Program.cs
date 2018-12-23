0f5dfde9e067d43472d9f6cbe01e440eab0d0a85
cristiano motta <cristiano.motta@hotmail.it>
2018-12-23 14:20:10 +0100

    Added Command Pattern

---
+++ CommandPattern/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace CommandPattern
+{
+    /* Command Pattern
+     
+     Il pattern Command (noto anche come Action o Transaction) permette di inoltrare richieste ad oggetti 
+     senza conoscere assolutamente nulla dell’operazione da eseguire o del destinatario della richiesta. 
+     Questo è possibile per il fatto che il pattern in questione tratta la richiesta come un oggetto differente 
+     rispetto sia al richiedente che all’oggetto destinatario. 
+     
+     Questo oggetto specifica l’azione da svolgere sul destinatario, sfruttandone i comportamenti in modo tale da 
+     poter portare a termine la richiesta.
+
+     Il pattern Command permette quindi di incapsulare una richiesta in un oggetto permettendo al client 
+     di inoltrare richieste di varia natura, anche in funzione di destinatari diversi. 
+     
+     Il pattern in questione può essere applicato:
+        
+        - per parametrizzare gli oggetti rispetto ad una azione da compiere
+        - per specificare, accodare ed eseguire svariate richieste in tempi diversi, anche trasferendo 
+          un comando da un contesto di esecuzione ad un altro;
+
+        - per consentire l’annullamento delle operazioni eseguite (undo, rollback), mantenendo preventivamente lo stato 
+          per annullare gli effetti dei comandi stessi.
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICommand[] commands =
+            {
+                new RegisterPersonCommand(),
+                new UnregisterPersonCommand()
+            };
+
+            var person = new PersonInfo()
+            {
+                FirstName = "Cristiano",
+                LastName = "Motta",
+                Id = 1
+            };
+
+            foreach (var command in commands)
+            {
+                command.Execute(person);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class PersonInfo
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+
+    public interface ICommand
+    {
+        bool Execute(PersonInfo personInfo);
+    }
+
+    public class RegisterPersonCommand : ICommand
+    {
+        public virtual bool Execute(PersonInfo personInfo)
+        {
+            Console.WriteLine($"Person {personInfo.ToString()} Registered succesfully");
+            return true;
+        }
+    }
+
+    public class UnregisterPersonCommand : ICommand
+    {
+        public virtual bool Execute(PersonInfo personInfo)
+        {
+            Console.WriteLine($"Person {personInfo.ToString()} Unregistered succesfully");
+            return true;
+        }
+    }
+}
---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace FactoryPattern
+{
+    /*  Factory Method Pattern
+
+        Nella programmazione ad oggetti, il Factory Method è uno dei design pattern fondamentali per l'implementazione 
+        del concetto di factory. Come altri pattern creazionali, esso indirizza il problema della creazione 
+        di oggetti senza specificarne l'esatta classe. 
+        
+        Questo pattern raggiunge il suo scopo fornendo un'interfaccia per creare un oggetto, 
+        ma lascia che le sottoclassi decidano quale oggetto istanziare.
+
+        La creazione di un oggetto può, spesso, richiedere processi complessi la cui collocazione all'interno 
+        della classe di composizione potrebbe non essere appropriata. 
+        Esso può, inoltre, comportare la duplicazione di codice, richiedere informazioni non accessibili alla classe 
+        di composizione, o non provvedere un sufficiente livello di astrazione. 
+        Il factory method indirizza questi problemi definendo un metodo separato per la creazione degli oggetti; 
+        tale metodo può essere ridefinito dalle sottoclassi per definire il tipo derivato di prodotto 
+        che verrà effettivamente creato.
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes =
+                new Shape[]
+                {
+                    ShapeCreator.CreateShape(ShapeType.Circle),
+                    ShapeCreator.CreateShape(ShapeType.Rectangle)
+                };
+
+            foreach (Shape s in shapes)
+                s.Draw();
+
+            Console.ReadLine();
+        }
+    }
+
+    public enum ShapeType
+    {
+        Rectangle = 1,
+        Circle = 2
+    }
+
+    // Posso usare una interfaccia oppure una classe astratta
+    public abstract class Shape
+    {
+        public abstract void Draw();
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Rectangle");
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Circle");
+        }
+    }
+
+    public class ShapeCreator
+    {
+        public static Shape CreateShape(ShapeType type)
+        {
+            switch (type)
+            {
+                case ShapeType.Rectangle:
+                    return new Rectangle();
+                case ShapeType.Circle:
+                    return new Circle();
+                default:
+                    throw new ArgumentException("type");
+            }
+        }
+    }
+}
