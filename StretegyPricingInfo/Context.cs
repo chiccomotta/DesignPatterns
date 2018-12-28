7e2706b2f85111989ae0526ad5ad7e42cabc15dc
cristiano motta <cristiano.motta@hotmail.it>
2018-12-28 22:25:38 +0100

    Refactoring

---
+++ StretegyPricingInfo/Context.cs
@@ -0,0 +1,19 @@
+﻿namespace StrategyPricingInfo
+{
+    public class Context
+    {
+        private readonly IExecuteStrategy Strategy;
+        private readonly PricingInfo PricingInfo;
+
+        public Context(IExecuteStrategy strategy, PricingInfo pricingInfo)
+        {
+            this.Strategy = strategy;
+            this.PricingInfo = pricingInfo;
+        }
+
+        public void Execute()
+        {
+            Strategy.Execute(PricingInfo);
+        }
+    }
+}
--- StretegyPricingInfo/Program.cs
+++ StretegyPricingInfo/Program.cs
@@ -29,21 +29,4 @@ namespace StrategyPricingInfo
             Console.ReadKey();
         }
     }
-
-    public class Context
-    {
-        private readonly IExecuteStrategy Strategy;
-        private readonly PricingInfo PricingInfo;
-
-        public Context(IExecuteStrategy strategy, PricingInfo pricingInfo)
-        {
-            this.Strategy = strategy;
-            this.PricingInfo = pricingInfo;
-        }
-
-        public void Execute()
-        {
-            Strategy.Execute(PricingInfo);
-        }
-    }
 }
