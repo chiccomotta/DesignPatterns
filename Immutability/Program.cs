57a0724ff5e35847a1d7d5b79c7af040c1220980
cristiano motta <cristiano.motta@hotmail.it>
2019-01-02 16:54:59 +0100

    Added immutability project

---
+++ Immutability/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Immutability
+{
+    // https://www.dotnetcurry.com/patterns-practices/1429/data-objects-csharp-fsharp
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Original Object
+            var existingDocument = new TextDocument("text_1", "Hello Wordl!");
+            var sameDocument = existingDocument;
+
+            // Same sreference
+            Console.WriteLine(object.ReferenceEquals(existingDocument, sameDocument));
+
+            // Modifico l'oggetto creandone uno nuovo con l'extension method With
+            var newDocument =
+                existingDocument     
+                    .WithIdentifier("text_2")
+                    .WithContent(existingDocument.Content + Environment.NewLine + "Thank you");
+
+            Console.WriteLine(object.ReferenceEquals(existingDocument, newDocument));
+
+            Console.WriteLine($"{newDocument.Identifier} , {newDocument.Content}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Immutability/TextDocument.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Immutability
+{
+    /* Make your data objects immutable
+        This means that once a data object is created, its contents will never change.
+        In C#, we can do this by making all properties read-only and providing a constructor to allow 
+        the initialization of these properties.
+    
+        Modifying immutable data objects
+        Immutable data objects cannot be modified. If we need to have a modified version of a data object, 
+        we have to create another one that has the same data, except for the part that we want to change. 
+        For example, if we want to append “Thank you” to the content of a document, 
+        we would create a new document.        
+     */
+
+    public class TextDocument
+    {
+        public TextDocument(string identifier, string content)
+        {
+            Identifier = identifier;
+            Content = content;
+        }
+
+        public string Identifier { get; }
+        public string Content { get; }
+
+        public TextDocument WithIdentifier(String newValue)
+        {
+            return new TextDocument(identifier: newValue, content: Content);
+        }
+
+        public TextDocument WithContent(String newValue)
+        {
+            return new TextDocument(identifier: Identifier, content: newValue);
+        }
+    }
+}
