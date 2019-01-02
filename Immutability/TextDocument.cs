a32f768f45e9307d4b839ed8b1599da3946a4826
cristiano motta <cristiano.motta@hotmail.it>
2019-01-02 21:07:25 +0100

    OK

--- Immutability/TextDocument.cs
+++ Immutability/TextDocument.cs
@@ -4,18 +4,6 @@ using System.Text;
 
 namespace Immutability
 {
-    /* Make your data objects immutable
-        This means that once a data object is created, its contents will never change.
-        In C#, we can do this by making all properties read-only and providing a constructor to allow 
-        the initialization of these properties.
-    
-        Modifying immutable data objects
-        Immutable data objects cannot be modified. If we need to have a modified version of a data object, 
-        we have to create another one that has the same data, except for the part that we want to change. 
-        For example, if we want to append “Thank you” to the content of a document, 
-        we would create a new document.        
-     */
-
     public class TextDocument
     {
         public string Identifier { get; }
