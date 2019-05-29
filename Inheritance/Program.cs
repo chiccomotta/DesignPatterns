e5f7184febde90281c5a788803acbecb954137c3
chiccomotta <cristiano.motta@hotmail.it>
2019-05-29 21:57:10 +0200

    Added Inheritance example

---
+++ Inheritance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        public class BaseC
+        {
+            public static int x = 55;
+            public static int y = 22;
+        }
+
+        public class DerivedC : BaseC
+        {
+            // Hide field 'x'.
+            public new static int x = 100;
+
+            static void Main()
+            {
+                // Display the new value of x:
+                Console.WriteLine(x);
+
+                // Display the hidden value of x:
+                Console.WriteLine(BaseC.x);
+
+                // Display the unhidden member y:
+                Console.WriteLine(y);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
