02341f43950c9713c3a76cfc4b49f143293e5bb1
chiccomotta <cristiano.motta@hotmail.it>
2019-07-24 21:45:23 +0200

    Added EqualityProject

---
+++ EqualityProject/EqualityProject/Equality.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualityProject
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //Allows to override method with argument type as Object only
+        public override bool Equals(Object obj)
+        {
+            if (obj == null)
+                return false;
+            var emp = (Employee)obj;
+            return emp.Id == Id && emp.Name == Name;
+        }
+    
+        //For hash based comparison
+        public override int GetHashCode() => new { Id, Name }.GetHashCode();
+    }
+
+    public class Person : IEquatable<Person>
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        //Flexibility to use argument type other than Object
+        public bool Equals(Person otherPerson)
+        {
+            if (otherPerson != null
+                && otherPerson.Age == Age && otherPerson.Name == Name)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //GetHashCode() method of Object base class is implemented for hash based comparison
+        public override int GetHashCode() => new { Age, Name }.GetHashCode();
+    }
+
+    /// <summary>
+    /// External Class which implements IEqualityComparer to compare equality of two objects of a type.
+    /// </summary>
+    public class StudentComparer : IEqualityComparer<Student>
+    {
+        public bool Equals(Student x, Student y)
+        {
+            if (x != null && y != null)
+            {
+                if (x.StudentId == y.StudentId && x.Name == y.Name)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public int GetHashCode(Student obj) => new { obj.StudentId, obj.Name }.GetHashCode();
+    }
+}
---
+++ EqualityProject/EqualityProject/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EqualityProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Expected behaviour with value types
+            var numList = new List<int>() { 4, 5 };
+            var numbers = new List<int>() { 3, 4 };
+            var result = numList.Intersect(numbers);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            
+            //Problem with object comparison
+            var students = new List<Student>();
+            var girl = new Student() { Name = "Simran", StudentId = 4 };
+            var sameGirl = new Student() { Name = "Simran", StudentId = 4 };
+            students.Add(girl);
+            Console.WriteLine("Default equality : {0}", students.Contains(sameGirl));
+
+            //Equality using overridden methods - Equal and GetHashCode of Object
+            var emp1 = new Employee() { Id = 1, Name = "John" };
+            var emp2 = new Employee() { Id = 1, Name = "John" };
+            Console.WriteLine(emp1.Equals(emp2));
+
+            var empList = new List<Employee>();
+            empList.Add(emp1);
+            Console.WriteLine(empList.Contains(emp2));
+
+            var employees = new HashSet<Employee>();
+            employees.Add(emp2);
+            Console.WriteLine(employees.Contains(emp1));
+
+            //Equality using IEqualityComparer
+            var studentList = new List<Student>();
+            studentList.Add(girl);
+            var stuList = new List<Student>();
+            stuList.Add(sameGirl);
+            var commonStudents = studentList.Intersect(stuList, new StudentComparer()).ToList();
+
+            foreach (var student in commonStudents)
+            {
+                Console.WriteLine("Id: {0} , Name: {1}", student.StudentId, student.Name);
+            }
+
+            Console.WriteLine(studentList.Contains(sameGirl, new StudentComparer()));
+
+            //Equality using IEquatable
+            var person1 = new Person() { Age = 21, Name = "Alice" };
+            var person2 = new Person() { Age = 21, Name = "Alice" };
+            Console.WriteLine(person1.Equals(person2));
+
+            Console.ReadKey();
+        }
+    }
+}
